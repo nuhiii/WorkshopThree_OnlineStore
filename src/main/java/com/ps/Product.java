d9380857c16b4d16f12638625a89aea976d9c9d9
Nuhirath Rafthia <nr2793@columbia.edu>
2024-04-28 19:33:13 -0400

    add Product class and products.csv file

---
+++ src/main/java/com/ps/Product.java
@@ -0,0 +1,41 @@
+package com.ps;
+
+public class Product {
+    private String sku;
+    private String productName;
+    private double price;
+    private String department;
+
+    public Product(String sku, String productName, double price, String department) {
+        this.sku = sku;
+        this.productName = productName;
+        this.price = price;
+        this.department = department;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "sku='" + sku + '\'' +
+                ", productName='" + productName + '\'' +
+                ", price=" + price +
+                ", department='" + department + '\'' +
+                '}';
+    }
+}
